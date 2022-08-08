1217098079404988691e206da7ba43424f7845a9
Evgeny <mishkin-583@mail.ru>
2022-08-08 19:19:30 +0300

    create variable moviedb

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+ import * as evFunctions from "./modules/functions.js";
+
+ evFunctions.isWebp();
+
+ import Swiper, { Navigation, Pagination } from 'swiper';
+
+ const swiper = new Swiper();
+
+
+
