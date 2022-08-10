4856058ae92e6260dda31236d8630798bb3cc67d
Evgeny <mishkin-583@mail.ru>
2022-08-10 21:19:59 +0300

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -8,4 +8,25 @@
  const swiper = new Swiper();
 
 
+ let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+    let personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+};
 
+let a = prompt('Один из последних просмотренныхфильмов?', ''),
+    b = +prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренныхфильмов?', ''),
+    d = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;  
+personalMovieDB.movies[c] = d;
+
+ console.log(personalMovieDB);
+
+
+console.log('hello');
