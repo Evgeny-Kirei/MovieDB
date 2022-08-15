090dba70d204c1a460569058719c76eb34dcdfa6
Evgeny <mishkin-583@mail.ru>
2022-08-15 18:20:28 +0300

    third commit

--- src/js/app.js
+++ src/js/app.js
@@ -18,15 +18,32 @@
         privat: false
 };
 
-let a = prompt('Один из последних просмотренныхфильмов?', ''),
-    b = +prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренныхфильмов?', ''),
-    d = +prompt('На сколько оцените его?', '');
+for (let i = 0; i < 1; i++) {
+    let a = prompt('Один из последних просмотренныхфильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+   
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+      i--;
+    }
+  
+}  
+   if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+  } else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+  } else {
+    console.log('Произошла ошибка');
+  }
+    
+console.log(personalMovieDB);
+  
 
-personalMovieDB.movies[a] = b;  
-personalMovieDB.movies[c] = d;
-
- console.log(personalMovieDB);
 
 
-console.log('hello');
+ 
+
+
